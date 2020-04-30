7427e7cad0ab751c60e28c70daf6afa43c3902e3
Rodrigo Hoshino Okiyama <hoshino.rodrigo@yahoo.com.br>
2020-04-29 22:02:02 -0300

    Add the new routes

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,13 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import CustomerController from './app/controllers/CustomerController';
+import EggController from './app/controllers/EggController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
 import CargoPackingController from './app/controllers/CargoPackingController';
-// import CargoPackingController from './app/controllers/CargoPackingController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,9 +18,12 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 routes.post('/customers', CustomerController.store);
+routes.post('/eggs', EggController.store);
 
 // Global middleware
 routes.use(authMiddleware);
+
+routes.get('/eggs', EggController.index);
 
 routes.post('/cargo-packing', CargoPackingController.store);
 routes.get('/cargo-packing', CargoPackingController.index);
@@ -33,8 +36,5 @@ routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
-
-// routes.post('/cargo-packing', CargoPackingController.store);
-// routes.get('/cargo-packing', CargoPackingController.index);
 
 export default routes;
