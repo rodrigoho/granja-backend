a89005df6b9c4244643efd767d6d4f73657f04b7
Rodrigo Hoshino Okiyama <hoshino.rodrigo@yahoo.com.br>
2020-09-05 13:57:35 -0300

    Add socket.io for notifications

--- src/server.js
+++ src/server.js
@@ -1,3 +1,26 @@
 import app from './app';
 
-app.listen(3333);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+  // io.emit('msg', { author: '[System]', datetime: new Date(), text: 'Hi, from socket server!', avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Wrench_font_awesome.svg/1024px-Wrench_font_awesome.svg.png' });
+  socket.on('user', (user) => {
+    io.emit('msg', {
+      author: '[System]',
+      datetime: new Date(),
+      text: `${user.user} just joined us on this chat room!`,
+      avatar:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Wrench_font_awesome.svg/1024px-Wrench_font_awesome.svg.png',
+    });
+  });
+  socket.on('msg', (msg) => {
+    console.log(msg);
+    io.emit('msg', msg);
+  });
+});
+
+http.listen(3333, () => {
+  console.log('Listening on port 3333...');
+});
+// app.listen(3333);
